28715eb0270edbb21d6acc8a943c1750083f458e
Timothy Cole <tim@timcole.me>
2018-07-22 05:59:24 -0400

    Setting package loading/getting private settings from datastore

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 
+	config "github.com/TimothyCole/timcole.me/settings"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"google.golang.org/appengine"
@@ -12,12 +13,18 @@ import (
 func main() {
 	router := mux.NewRouter()
 
+	settings := config.InitSettings()
+
 	var static = http.StripPrefix("/assets", http.FileServer(http.Dir("./build")))
 	router.PathPrefix("/assets").Handler(static)
 
 	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./build/index.html")
 	})
+
+	router.HandleFunc("/123", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(settings.Get("GAMER_BOY")))
+	}).Methods("GET")
 
 	// Respond to App Engine and Compute Engine health checks.
 	// Indicate the server is healthy.
---
+++ settings/settings.go
@@ -0,0 +1,58 @@
+package settings
+
+import (
+	"context"
+	"sync"
+
+	"cloud.google.com/go/datastore"
+)
+
+// Settings are your settings
+type Settings struct {
+	mutex sync.Mutex
+	kvs   map[string]string
+}
+
+type rawKVS struct {
+	Key   string `datastore:"key"`
+	Value string `datastore:"value"`
+}
+
+// InitSettings creates the inital settings
+func InitSettings() *Settings {
+	var s = &Settings{}
+	s.Load()
+
+	return s
+}
+
+// Load soads the data from datastore
+func (s *Settings) Load() map[string]string {
+	var kvs = map[string]string{}
+	var rawkvs []*rawKVS
+
+	ctx := context.Background()
+
+	client, err := datastore.NewClient(ctx, "timcole-me")
+	if err != nil {
+		panic(err)
+	}
+
+	query := datastore.NewQuery("Settings")
+	_, err = client.GetAll(ctx, query, &rawkvs)
+
+	for _, r := range rawkvs {
+		kvs[r.Key] = r.Value
+	}
+
+	s.mutex.Lock()
+	s.kvs = kvs
+	s.mutex.Unlock()
+
+	return kvs
+}
+
+// Get gets the value from a key
+func (s *Settings) Get(key string) string {
+	return s.kvs[key]
+}
