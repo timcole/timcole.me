f47e6c549e9cd8694e0e310f2c148dd647be72fd
Timothy Cole <tim@timcole.me>
2018-09-12 16:54:15 -0400

    Emotes endpoint cache

--- stream_emotes.go
+++ stream_emotes.go
@@ -5,14 +5,18 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/machinebox/graphql"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/memcache"
 )
 
 // GQLEmotes is the response from Twitch's GraphQL for Emotes
 type GQLEmotes struct {
 	CurrentUser struct {
 		DisplayName string `json:"displayName"`
+		Cache       int64  `json:"cache"`
 		EmoteSets   []struct {
 			Emotes []struct {
 				ID    string      `json:"id"`
@@ -27,6 +31,26 @@ type GQLEmotes struct {
 
 // GetEmotes returns WeetBot's Twitch emotes
 func GetEmotes(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.BackgroundContext()
+	cacheKey := "WeetBot::Emotes"
+	var data GQLEmotes
+
+	if cache, err := memcache.Get(ctx, cacheKey); err == nil {
+		if err := json.Unmarshal(cache.Value, &data.CurrentUser); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"status": 500, "error": "StatusInternalServerError"}`))
+			log.Fatal(err)
+			return
+		}
+
+		// Has the cache expired yet?
+		if data.CurrentUser.Cache-time.Now().UTC().Unix() > 0 {
+			// Cache is okay
+			w.Write(cache.Value)
+			return
+		} // Cache is expired
+	}
+
 	req := graphql.NewRequest(`
 		query {
 			currentUser {
@@ -45,7 +69,6 @@ func GetEmotes(w http.ResponseWriter, r *http.Request) {
 	`)
 	req.Header.Set("Authorization", "OAuth "+settings.Get("TWITCH_OAUTH_TOKEN"))
 
-	var data GQLEmotes
 	if err := gql.Run(context.Background(), req, &data); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(`{"status": 500, "error": "StatusInternalServerError"}`))
@@ -53,6 +76,14 @@ func GetEmotes(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	resp, _ := json.Marshal(data.CurrentUser.EmoteSets)
+	data.CurrentUser.Cache = time.Now().UTC().Add(30 * time.Minute).Unix()
+	resp, _ := json.Marshal(data.CurrentUser)
+
+	item := &memcache.Item{
+		Key:   cacheKey,
+		Value: resp,
+	}
+	memcache.Set(ctx, item)
+
 	w.Write(resp)
 }
