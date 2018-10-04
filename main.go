a8f50843871b5fe0afe5133c91b32bc70fdc6e33
Timothy Cole <tim@timcole.me>
2018-10-04 15:10:24 -0400

    Fixed twitch-stats CORS

--- main.go
+++ main.go
@@ -81,6 +81,13 @@ func main() {
 	streamAdmin.HandleFunc("/message", stream.SetStreamMessage).Methods("POST")
 
 	// tcole.me Handlers
+	tcoleme.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			next.ServeHTTP(w, r)
+		})
+	})
 	tcoleme.HandleFunc("/twitch-stats", TwitchStats).Methods("GET")
 
 	// modest.land Handlers
