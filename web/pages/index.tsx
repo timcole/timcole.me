b8342e13b03107bbeceece6de2898a9b60bb8811
Timothy Cole <tim@timcole.me>
2019-02-25 05:24:13 -0500

    WIP: Pinned Repos

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/layout'
 import Header from '../components/header'
 import Bio from '../components/bio'
 import Socials from '../components/socials'
+import Repo from "../components/repo";
 import Footer from '../components/footer'
 
 import "../styles/index.scss"
@@ -16,12 +17,25 @@ class Index extends Component<any> {
 		this.store = props.store
 	}
 
+	private repos = [
+		{ name: "timcole.me", desc: "asdasd asd asd asd asd asd asd", lang: "Go" },
+		{ name: "timcole.me", desc: "asdasd asd asd asd asd asd asd", lang: "Go" },
+		{ name: "timcole.me", desc: "asdasd asd asd asd asd asd asd", lang: "Go" },
+		{ name: "timcole.me", desc: "asdasd asd asd asd asd asd asd", lang: "Go" },
+	]
+
 	render() {
 		return (
 			<Layout className="homepage">
 				<div className="header"><Header className="container" store={this.store} /></div>
-				<div className="setup"><div className="bio"><Bio className="container" /></div></div>
+				<div className="setup"><div className="bio"><Bio className="narrow" /></div></div>
 				<Socials className="socials" />
+				<div className="container">
+					<h1 className="narrow">Pinned Repositories</h1>
+					<div className="repos">
+						{this.repos.map((repo: {}) => <Repo info={repo} />)}
+					</div>
+				</div>
 				<Footer />
 			</Layout>
 		)
