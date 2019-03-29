f2bc821863bbfaec10e84452dfe593bf61666cea
Timothy Cole <tim@timcole.me>
2019-03-28 22:13:24 -0400

    Change 404 page to get errors

--- web/pages/_error.tsx
+++ web/pages/_error.tsx
@@ -6,7 +6,13 @@ import Footer from '../components/footer';
 
 import "../styles/_error.scss";
 
-class Error extends Component {
+class Error extends Component<any> {
+	static getInitialProps({ res, err }) {
+		if (err) console.log({ err })
+		const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+		return { statusCode };
+	}
+
 	private store: any
 	constructor (props: any) {
 		super(props)
@@ -20,8 +26,8 @@ class Error extends Component {
 				<div className="_error">
 					<div className="header"><Header className="container" store={this.store} /></div>
 					<div className="container error">
-						<h1>404</h1>
-						<h2>Not Found</h2>
+						<h1>{this.props.statusCode}</h1>
+						<h2>{this.props.statusCode == "404" ? "Not Found" : "Error"}</h2>
 					</div>
 					<Footer />
 				</div>
