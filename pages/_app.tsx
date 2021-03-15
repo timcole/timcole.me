495c49ba94fb29a5220967498648bc63c957d22a
Timothy Cole <tim@timcole.me>
2021-03-15 04:39:50 -0400

    sans-serif fallback

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   html, body {
-    font-family: 'Operator Mono', 'dm';
+    font-family: 'Operator Mono', 'dm', sans-serif;
     padding: 0;
     margin: 0;
     background: var(--background);
