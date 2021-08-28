71e5ddba8fde750c193915b373484b2350097973
Timothy Cole <tim@timcole.me>
2021-08-27 21:21:00 -0400

    Fixed unnecessary rerender in hls component
    - This was causing stream network requests to get interrupted making the
      the video stream unplayable and throwing errors in Chromium.
      - https://github.com/devcshort/react-hls/issues/30

--- components/hls.tsx
+++ components/hls.tsx
@@ -1,74 +1,65 @@
 import {
   FC,
   forwardRef,
+  memo,
   RefObject,
-  useEffect,
+  useMemo,
   useRef,
   useState,
   VideoHTMLAttributes,
 } from 'react';
-import ReactHlsPlayer, { HlsPlayerProps } from 'react-hls-player';
+import { HlsPlayerProps } from 'react-hls-player';
 import { Orb } from './status';
+import dynamic from 'next/dynamic';
+
+const ReactHlsPlayer = dynamic(() => import('react-hls-player'));
 
 type Props = { stream: string; image: string };
 
-const Player = forwardRef<
-  HTMLVideoElement,
-  VideoHTMLAttributes<HTMLVideoElement>
->((props, ref) => {
-  const [hlsSupport, setHlsSupport] = useState<boolean>(false);
-  useEffect(() => {
-    const video = document.createElement('video');
-    setHlsSupport(Boolean(video.canPlayType('application/vnd.apple.mpegurl')));
-  }, []);
-
-  if (hlsSupport)
-    return (
-      <video
-        ref={ref}
-        onLoadedMetadata={async () => {
-          (ref as RefObject<HTMLVideoElement>).current.play();
-        }}
-        {...props}
-      ></video>
-    );
-
-  return (
-    <ReactHlsPlayer
-      playerRef={ref as RefObject<HTMLVideoElement>}
-      hlsConfig={{
-        maxLatency: 30,
-        maxLoadingDelay: 10,
-        minAutoBitrate: 0,
-        lowLatencyMode: true,
-        progressive: false,
-      }}
-      {...(props as HlsPlayerProps)}
-    />
-  );
-});
+const Player = memo(
+  forwardRef<HTMLVideoElement, VideoHTMLAttributes<HTMLVideoElement>>(
+    (props, ref) =>
+      document
+        .createElement('video')
+        .canPlayType('application/vnd.apple.mpegurl') ? (
+        <video
+          ref={ref}
+          onLoadedMetadata={async () => {
+            (ref as RefObject<HTMLVideoElement>).current.play();
+          }}
+          {...props}
+        ></video>
+      ) : (
+        <ReactHlsPlayer
+          playerRef={ref as RefObject<HTMLVideoElement>}
+          hlsConfig={{
+            maxLatency: 60,
+            maxLoadingDelay: 10,
+            minAutoBitrate: 0,
+            lowLatencyMode: true,
+          }}
+          {...(props as HlsPlayerProps)}
+        />
+      ),
+  ),
+  () => true,
+);
 
 const Stream: FC<Props> = ({ stream, image }) => {
   const ref = useRef<HTMLVideoElement>();
   const [live, setLive] = useState<'live' | 'offline' | 'loading'>('loading');
 
-  useEffect(() => {
-    if (!ref?.current) return;
+  const videoStart = () => setLive('live');
+  const videoBuff = () => setLive('loading');
+  const videoEnd = () => setLive('offline');
 
-    const videoStart = () => setLive('live');
-    const videoBuff = () => setLive('loading');
-    const videoEnd = () => setLive('offline');
-
-    ref.current.addEventListener('play', videoStart);
-    ref.current.addEventListener('hlsError', videoBuff);
-    ref.current.addEventListener('ended', videoEnd);
-
-    return () => {
-      ref.current.removeEventListener('play', videoStart);
-      ref.current.removeEventListener('hlsError', videoBuff);
-      ref.current.removeEventListener('ended', videoEnd);
-    };
-  }, [ref]);
+  const orb = useMemo(() => {
+    return live === 'live'
+      ? ['#f04747', 'Live']
+      : live === 'offline'
+      ? ['#747f8d', 'Stream Offline']
+      : ['#faa61a', 'Buffering'];
+  }, [live]);
 
   return (
     <div>
@@ -79,6 +70,9 @@ const Stream: FC<Props> = ({ stream, image }) => {
         muted
         height={200}
         width={355.55}
+        onPlay={videoStart}
+        onEnded={videoEnd}
+        onError={videoBuff}
         onClick={({ target }) => {
           if (!window.screenTop && !window.screenY) document.exitFullscreen();
           else (target as HTMLVideoElement)?.requestFullscreen();
@@ -86,21 +80,11 @@ const Stream: FC<Props> = ({ stream, image }) => {
         autoPlay
         playsInline
       />
-      <Orb
-        color={
-          live === 'live'
-            ? '#f04747'
-            : live === 'offline'
-            ? '#747f8d'
-            : '#faa61a'
-        }
-      >
-        {live === 'live' && <p>Live</p>}
-        {live === 'offline' && <p>Stream Offline</p>}
-        {live === 'loading' && <p>Buffering...</p>}
+      <Orb color={orb[0]}>
+        <p>{orb[1]}</p>
       </Orb>
     </div>
   );
 };
 
-export default Stream;
+export default memo(Stream);
