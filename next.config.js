490f4d15a98739a2c648bb56ee946dfcf97dcf0c
Timothy Cole <tim@timcole.me>
2021-11-04 22:28:57 -0400

    Update packages and redirect /freshtok

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   images: {
-    domains: ['cdn.t.pics', 'emojipedia-us.s3.dualstack.us-west-1.amazonaws.com', 'i.scdn.co', 'cdn.discordapp.com'],
+    domains: [
+      'cdn.t.pics',
+      'emojipedia-us.s3.dualstack.us-west-1.amazonaws.com',
+      'i.scdn.co',
+      'cdn.discordapp.com',
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/freshtok',
+        destination: 'https://top.gg/bot/890842672133201950',
+        permanent: false,
+      },
+    ];
   },
 };
