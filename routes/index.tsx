d1d380b1556364ea670171d9f5aa96d05c58e429
Timothy Cole <tim@timcole.me>
2022-07-10 16:05:39 -0400

    Changed booster internal url

--- routes/index.tsx
+++ routes/index.tsx
@@ -79,7 +79,7 @@ export const handler: Handlers<Props> = {
   async GET(_, ctx) {
     const {
       data: { launches },
-    } = await fetch(`http://booster.spaceflight:3000`, {
+    } = await fetch(`http://booster-external-service.ix-booster`, {
       method: "POST",
       headers: {
         "Content-type": "application/json",
