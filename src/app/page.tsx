1e55754c1f40f056bdf471bc704c17b072f5b798
Timothy Cole <tim@timcole.me>
2024-06-12 15:42:38 -0400

    Fix age cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Lanyard from '@/components/Lanyard';
 import AboutMe from '@/components/about';
 import Positions from '@/components/positions';
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <div className="flex-1 flex flex-col px-3">
