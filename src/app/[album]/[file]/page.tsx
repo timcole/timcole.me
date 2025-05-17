75975075876d94693fa4a7ada9affced3d87208a
Timothy Cole <tim@timcole.me>
2025-05-16 23:56:17 -0400

    Some tweaks to image pages

--- src/app/[album]/[file]/page.tsx
+++ src/app/[album]/[file]/page.tsx
@@ -48,14 +48,14 @@ export default async function Album({ params }: Props) {
       <div className="w-full flex-1 justify-center align-middle flex relative">
         <Link
           href={`/${album}`}
-          className="absolute z-10 opacity-0 hover:opacity-100 transition-opacity left-2 top-2 bg-primary-900 p-2 rounded-full text-primary-50"
+          className="absolute z-20 opacity-0 hover:opacity-100 transition-opacity left-2 top-2 bg-primary-900 p-2 rounded-full text-primary-50"
         >
           <X />
         </Link>
         {photo?.prevFile && (
           <Link
             href={`/${album}/${photo.prevFile}`}
-            className="absolute z-10 opacity-60 hover:opacity-100 transition-opacity left-2 top-1/2 -translate-y-1/2 bg-primary-900 p-2 rounded-full text-primary-50"
+            className="absolute z-20 opacity-60 hover:opacity-100 transition-opacity left-2 top-1/2 -translate-y-1/2 bg-primary-900 p-2 rounded-full text-primary-50"
           >
             <ChevronLeft />
           </Link>
@@ -63,20 +63,25 @@ export default async function Album({ params }: Props) {
         {photo?.nextFile && (
           <Link
             href={`/${album}/${photo.nextFile}`}
-            className="absolute z-10 opacity-60 hover:opacity-100 transition-opacity right-2 top-1/2 -translate-y-1/2 bg-primary-900 p-2 rounded-full text-primary-50"
+            className="absolute z-20 opacity-60 hover:opacity-100 transition-opacity right-2 top-1/2 -translate-y-1/2 bg-primary-900 p-2 rounded-full text-primary-50"
           >
             <ChevronLeft className="rotate-180" />
           </Link>
         )}
-        <div key={`img-${album}-${file}`} className="relative h-full w-fit">
+        <div key={`img-${album}-${file}`} className="relative">
           <img
             src={`${base_url}/${album}/${file}`}
             alt={file}
-            className="w-full h-full object-contain"
+            className="w-full h-full object-contain relative z-10 shadow-md"
+          />
+          <img
+            src={`${base_url}/${album}/${file}`}
+            alt={file}
+            className="w-full h-full absolute top-0 object-cover blur-lg opacity-20"
           />
         </div>
       </div>
-      <div className="bg-primary-800 w-full lg:w-lg h-full min-h-fit p-4 flex flex-col gap-8">
+      <div className="bg-primary-800 w-full lg:w-md h-full min-h-fit p-4 flex flex-col gap-8 z-10">
         <div className="flex justify-between">
           <h1>{file}</h1>
           <Link href={`/${album}`} className="text-primary-50">
